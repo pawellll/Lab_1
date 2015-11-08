bc1e597e0e8b907a10dcfa30a8d06bb7f68dbd0e
Pawel Peksa <pawel.peksa@fis.agh.edu.pl>
2015-11-08 15:13:24 +0100

    finish problem 2

--- problem2/main.py
+++ problem2/main.py
@@ -1,17 +1,30 @@
-import xml.etree.ElementTree
+import xml.etree.ElementTree as ET
+import sys
+from pprint import pprint
 
-if __name__ == "__main__":
-	print("Lab 01 | Problem 2")
+if __name__ == '__main__':
+	print('Lab 01 | Problem 2')
 	
-	parsedBooks = xml.etree.ElementTree.parse('Books.xml').getroot()
+	parsedBooks = None
+	try:
+		parsedBooks = ET.parse('Books.xml').getroot()
+	except IOError as e:
+		print "Failed: %s" %e
+		sys.exit()
+
 	booksList = []
+
 	for book in parsedBooks.findall('book'):
+		#retrieve information from xml
 		bookID = book.get('id')
 		author = book.find('author').text
 		title = book.find('title').text
 		genre = book.find('genre').text
 		price = book.find('price').text
-		bookTuple = [bookID,author]
+		publishDate = book.find('publish_date').text
+		description = book.find('description').text
+
+		bookTuple = [bookID,author,title,genre,price,publishDate,description]
 		booksList.append(bookTuple)
 		
-	print(booksList)
+	pprint(booksList)
