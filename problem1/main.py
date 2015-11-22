e2d50bfa9681af66a76cfe4737adb227fd314d1f
pawellll <pmpeksa@gmail.com>
2015-11-22 15:08:08 +0100

    add .gitignore, change to python3

--- problem1/main.py
+++ problem1/main.py
@@ -1,56 +1,70 @@
-import sys, time, subprocess
+import sys
+import time
+import logging
+import os
 from Daemon import Daemon
- 
+
+
 class SystemMonitorDaemon(Daemon):
-	def run(self):
-		while True:
-			time.sleep(1)
-			logState()
+    def __init__(self, pidfile):
+        super().__init__(pidfile)
+        self._cpu = False
+        self._network = False
+        self._memory = False
 
-	"""log current state of system"""
-	def logState(self):
-		logFile = super().logFile
-		logFile.write('logging')
-		
-	"""set mode for system monitor c=CPU,n=NETWORK,m=MEMORY"""	
-	def setMode(self,mode):
-		self._cpu = True if 'c' in mode else False
-		self._netowrk = True if 'n' in mode else False
-		self._memory = True if 'm' in mode else False
-  
+    def run(self):
+        while True:
+            time.sleep(1)
 
-def badUsageHandler():
-	print('usage: %s start|stop|restart [OPTIONS]' % sys.argv[0])
-	print('Options description:')
-	print('c  ,CPU')
-	print('n  ,NETWORK')
-	print('m  ,memory')
-	print('Example:')
-	print('main.py start cn')
-	sys.exit(2)
+    """set mode for system monitor c=CPU,n=NETWORK,m=MEMORY"""
+    def set_mode(self, mode):
+        self._cpu = True if 'c' in mode else False
+        self._network = True if 'n' in mode else False
+        self._memory = True if 'm' in mode else False
+
+    def setup_logging(self, log_path, logging_level, logging_format):
+        # remove log file if exists
+        try:
+            os.remove(log_path)
+        except OSError:
+            pass
+
+        logging.basicConfig(filename=log_path, level=logging_level, format=logging_format)
+        logging.info("Logging has been initialised")
+
+
+def bad_usage_handler():
+    print('usage: %s start|stop|restart [OPTIONS]' % sys.argv[0])
+    print('Options description:')
+    print('c  ,CPU')
+    print('n  ,NETWORK')
+    print('m  ,memory')
+    print('Example:')
+    print('main.py start cn')
+    sys.exit(2)
 
 
 if __name__ == "__main__":
-	daemon = SystemMonitorDaemon('/tmp/systemMonitorDaemon.pid')
-	argLen = len(sys.argv)
-	if argLen == 2:
-		if 'start' == sys.argv[1]:
-			print('Starting daemon in default mode (Network+Memory+Cpu)')
-			daemon.setMode('cnm')
-			daemon.start()
-		elif 'stop' == sys.argv[1]:
-			daemon.stop()
-		elif 'restart' == sys.argv[1]:
-			daemon.restart()
-		else:
-			print "Unknown command"
-			sys.exit(2)
-	elif argLen == 3:
-		if 'start' == sys.argv[1]:
-			daemon.setMode(sys.argv[2])
-			print('Starting daemon in mode:%s' % sys.argv[2])
-			daemon.start()
-		else:
-			badUsageHandler()
-	else:
-		badUsageHandler()
+    daemon = SystemMonitorDaemon('/tmp/systemMonitorDaemon.pid')
+    argLen = len(sys.argv)
+    if argLen == 2:
+        if 'start' == sys.argv[1]:
+            print('Starting daemon in default mode (Network+Memory+Cpu)')
+            daemon.setMode('cnm')
+            daemon.start()
+        elif 'stop' == sys.argv[1]:
+            daemon.stop()
+        elif 'restart' == sys.argv[1]:
+            daemon.restart()
+        else:
+            print("Unknown command")
+            sys.exit(2)
+    elif argLen == 3:
+        if 'start' == sys.argv[1]:
+            daemon.setMode(sys.argv[2])
+            print('Starting daemon in mode:%s' % sys.argv[2])
+            daemon.start()
+        else:
+            bad_usage_handler()
+    else:
+        bad_usage_handler()
